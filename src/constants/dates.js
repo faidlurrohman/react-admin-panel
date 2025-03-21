682d52bd6dcd7c7a6ee7c071237f21e5630ddd39
faidlurrohman <faidrohman@outlook.com>
2025-03-21 13:50:01 +0700

    remove months

--- src/constants/dates.js
+++ src/constants/dates.js
@@ -2,18 +2,4 @@ export const DATES = {
   UTC: "Asia/Jakarta",
   DB: "YYYY-MM-DD",
   VIEW: "DD MMMM YYYY",
-  MONTHS: [
-    "Januari",
-    "Februari",
-    "Maret",
-    "April",
-    "Mei",
-    "Juni",
-    "Juli",
-    "Agustus",
-    "September",
-    "Oktober",
-    "November",
-    "Desember",
-  ],
 };
