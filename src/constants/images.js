1951377d18d07adb2c39f4e22a6f25ff06a44f20
faidlurrohman <faidrohman@outlook.com>
2025-03-21 13:50:11 +0700

    add flag

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,11 @@
 import NotFound from "assets/images/404.webp";
 import LOGO_HERE from "assets/images/logo-here.png";
+import EN from "assets/images/countries/en.png";
+import ID from "assets/images/countries/id.png";
 
 export const IMAGE = {
   404: NotFound,
   LOGO_HERE: LOGO_HERE,
+  EN: EN,
+  ID: ID,
 };
